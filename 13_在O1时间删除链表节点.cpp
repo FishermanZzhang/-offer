fcc68f614e75bf5918952776c0494cbb6a4014a1
zhangzhen <1099905725@qq.com>
2016-07-08 22:46:26 +0800

    13 good question one more

---
+++ 13_在O1时间删除链表节点.cpp
@@ -0,0 +1,21 @@
+void DeleteNode(ListNode** head, ListNode* pdeltenode){
+	if (head == NULL || *head == NULL || pdeltenode == NULL) return;
+	if ((*head)->next == NULL && (*head) == pdeltenode){
+		delete pdeltenode;
+		*head == NULL;
+	}
+	else if(pdeltenode->next != NULL){
+		pdeltenode->val = pdeltenode->next->val;
+		ListNode* p = pdeltenode->next;
+		if (p == NULL){
+			pdeltenode->next == NULL;
+		}
+		else{
+			pdeltenode->next = p->next;
+		}
+		delete p;
+	}
+	else{
+		// 尾结点，从头遍历
+	}
+}
