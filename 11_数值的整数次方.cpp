de1fb02348bb76b5c8300b1949ac0dd6e5af4a78
zhangzhen <1099905725@qq.com>
2016-07-08 21:33:06 +0800

    11 ok

---
+++ 11_数值的整数次方.cpp
@@ -0,0 +1,10 @@
+double Power(double base, int exponent){
+	function<double(double,int)> mypower = [&mypower](double base, int x){
+		if (x == 0) return 1.0;
+		if (x == 1) return base;
+		double r = mypower(base, x / 2);
+		return r * r * ((x & 1) ? base : 1.0);
+	};
+	if (exponent < 0) return mypower(1.0 /base, -exponent);
+	return mypower(base, exponent);
+}
