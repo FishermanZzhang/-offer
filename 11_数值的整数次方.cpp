e91f69af737c4ad3b056b4fe5f8135c1cc9ced45
fishermanzhangzhen <1099905725@qq.com>
2016-07-08 21:47:16 +0800

    Update 11_数值的整数次方.cpp

--- 11_数值的整数次方.cpp
+++ 11_数值的整数次方.cpp
@@ -5,6 +5,7 @@ double Power(double base, int exponent){
 		double r = mypower(base, x / 2);
 		return r * r * ((x & 1) ? base : 1.0);
 	};
+	if(base == 0.0) return .0;
 	if (exponent < 0) return mypower(1.0 /base, -exponent);
 	return mypower(base, exponent);
 }
