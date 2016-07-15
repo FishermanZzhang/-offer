013494d2da508af0806785db570a18998e972073
zhangzhen <1099905725@qq.com>
2016-07-15 16:10:09 +0800

    08 ok

--- 08_旋转数组的最小数字.cpp
+++ 08_旋转数组的最小数字.cpp
@@ -37,3 +37,34 @@ int min2(vector<int>& nums){
 	}
 	return nums[left];
 }
+
+
+
+class solution8{
+public:
+	int minElement(vector<int>& nums){
+		int left = 0;
+		int right = nums.size() - 1;
+		int mid = -1;
+		auto getminbyliear = [nums](int start, int end){
+			int m = INT_MAX;
+			for (int i = start; i <= end; ++i){
+				m = std::min(m, nums[i]);
+			}
+			return m;
+		};
+		while (left <= right){
+			mid = left + ((right - left) >> 1);
+			if (nums[mid] == nums[right]){
+				return getminbyliear(left, right);
+			}
+			else if (nums[mid] > nums[right]){
+				left = mid + 1;
+			}
+			else{
+				right = mid;
+			}
+		}
+		return nums[left];
+	}
+};
