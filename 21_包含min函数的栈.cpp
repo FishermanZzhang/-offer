aec4a1b441d7796fa1815b6b0b1186bdfe2f1369
zhangzhen <1099905725@qq.com>
2016-07-17 15:16:57 +0800

    2nd one more

--- 21_包含min函数的栈.cpp
+++ 21_包含min函数的栈.cpp
@@ -1,1 +1,29 @@
-// one more
+template<typename T>
+class minStack{
+public:
+	minStack() : min_(INT_MAX){
+	}
+	void push(const T& t){
+		if (min_ > t){
+			stack_.push(min_);
+			min_ = t;
+		}
+		stack_.push(t);
+	}
+	const T& top() const{
+		return stack_.top();
+	}
+	const T& min() const{
+		return min_;
+	}
+	void pop(){
+		if (min_ == stack_.top()){
+			stack_.pop();
+			min_ = stack_.top();			
+		}
+		stack_.pop();
+	}
+private:
+	T min_;
+	std::stack<T> stack_;
+};
